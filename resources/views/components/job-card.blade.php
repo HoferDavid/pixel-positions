fb2b23cbdf335f31cdfa0aca0704838d1261fbe8
David Hofer <chassis.poppets0x@icloud.com>
2024-10-28 10:10:10 +0100

    first commit

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">laracasts</div>
+
+    <div class="py-8 font-bold">
+        <h3>video producer</h3>
+        <p>full time - from $60,000</p>
+    </div>
+    
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+        </div>
+        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
+        // './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        // './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'black': '#060606'
+            }
         },
     },
     plugins: [],
